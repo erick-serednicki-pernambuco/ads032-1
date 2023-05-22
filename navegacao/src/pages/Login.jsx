f35828270bd728849cb2ae5701c8bcb8b10b624a
erick-serednicki-pernambuco <pernambuco.erick@gmail.com>
2023-05-22 19:55:31 -0300

    pratica 2

--- navegacao/src/pages/Login.jsx
+++ navegacao/src/pages/Login.jsx
@@ -1,17 +1,38 @@
 import { useNavigate } from "react-router-dom";
+import { useRef, useState } from "react";
 
 export default function Login(props) {
     const navigate = useNavigate();
+    const refInputEmail = useRef();
+    const refInputSenha = useRef();
+    const [erroEmail, setErroEmail] = useState();
 
     function handleClick(event) {
         props.onLogin(event);
         navigate("/");
     }
 
+    function handleBLurInputEmail(event){
+        if(refInputEmail.current.value.length == 0){
+            setErroEmail("Email Obrigatorio");
+        }else{
+            setErroEmail(null)
+        }
+    }
+
     return (
         <>
             <h1>Login</h1>
-            <button onClick={handleClick}>Entrar</button>
+            <form>
+                <label htmlFor="email">Email</label>
+                <input tipe="email" id="email" name="email" ref={refInputEmail} onBlur={handleBLurInputEmail} />
+                {erroEmail && <p>{erroEmail}</p>}
+                <label htmlFor="senha">Senha</label>
+                <input tipe="password" id="senha" name="senha" ref={refInputSenha}  />
+                <button onClick={handleClick}>Entrar</button>
+               
+            </form>
+
         </>
     )
 }
